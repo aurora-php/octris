9dcb2b985d318a0218bfd7c737086abe65459831
Harald Lapp <harald@octris.org>
2014-10-11 17:53:00 +0200

    initial commit skeleton

---
+++ data/skel/cli/$module.php
@@ -0,0 +1,31 @@
+#!/usr/bin/env php
+<?php
+
+/*
+ * This file is part of the '{{$directory}}' package.
+ *
+ * (c) {{$company}}
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/*
+ * WARNING!!!
+ *
+ * MODIFICATION OF THIS FILE MAY LEAD TO UNEXPECTED BEHAVIOUR!
+ */
+
+/**
+ * Application launcher.
+ *
+ * @octdoc      h:{{$module}}/{{$module}}.php
+ * @copyright   copyright (c) {{$year}} by {{$company}}
+ * @author      {{$author}} <{{$email}}>
+ */
+/**/
+
+require_once(__DIR__ . '/libs/autoloader.class.php');
+
+$main = new {{$namespace}}\libs\main();
+$main->run();
