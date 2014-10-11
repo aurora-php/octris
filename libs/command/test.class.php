b0e4d1dd55fe94ba2becc3307525b091a57bb02d
Harald Lapp <harald@octris.org>
2014-10-11 02:05:24 +0200

    initial commit test command

---
+++ libs/command/test.class.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the 'octris' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace octris\command {
+    /**
+     * Text command
+     *
+     * @octdoc      c:command/test
+     * @copyright   copyright (c) 2014 by Harald Lapp
+     * @author      Harald Lapp <harald@octris.org>
+     */
+    class test extends \octris\command
+    /**/
+    {
+    }
+}
