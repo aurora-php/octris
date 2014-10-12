3918440566b0f30a44690994fc8e2774e644d492
Harald Lapp <harald@octris.org>
2014-10-12 23:14:45 +0200

    added method for parsing cli options

--- libs/main.class.php
+++ libs/main.class.php
@@ -53,6 +53,77 @@ namespace octris {
         }
 
         /**
+         * Parse command line options and return Array of them. The parameters are required to have
+         * the following format:
+         *
+         * - short options: -l -a -b
+         * - short options combined: -lab
+         * - short options with value: -l val -a val -b "with whitespace"
+         * - long options: --option1 --option2
+         * - long options with value: --option=value --option value --option "with whitespace"
+         *
+         * @octdoc  m:main/getOptions
+         * @param   array           $args               Allows to specify an array of arguments to use instead of global $argv.
+         * @return  array                               Parsed command line parameters.
+         */
+        public function getOptions(array $args = null)
+        /**/
+        {
+            if (is_null($args)) {
+                $args = $GLOBALS['argv'];
+                array_shift($args);
+            }
+ 
+            $opts = array();
+            $key  = '';
+            $idx  = 0;
+ 
+            foreach ($args as $arg) {
+                if (preg_match('/^-([a-zA-Z]+)$/', $arg, $match)) {
+                    // short option, combined short options
+                    $tmp  = str_split($match[1], 1);
+                    $opts = array_merge(array_combine($tmp, array_fill(0, count($tmp), true)), $opts);
+                    $key  = array_pop($tmp);
+                    
+                    continue;
+                } elseif (preg_match('/^--([a-zA-Z][a-zA-Z0-9]+)(=.*|)$/', $arg, $match)) {
+                    // long option
+                    $key  = $match[1];
+                    $opts = array_merge(array($key => true), $opts);
+ 
+                    if (strlen($match[2]) == 0) {
+                        continue;
+                    }
+ 
+                    $arg = substr($match[2], 1);
+                } elseif (strlen($arg) > 1 && substr($arg, 0, 1) == '-') {
+                    // invalid option format
+                    throw new \Exception('invalid option format "' . $arg . '"');
+                }
+ 
+                if ($key == '') {
+                    // no option name, add as numeric option
+                    $opts[$idx++] = $arg;
+                } else {
+                    if (!is_bool($opts[$key])) {
+                        // multiple values for this option
+                        if (!is_array($opts[$key])) {
+                            $opts[$key] = array($opts[$key]);
+                        }
+                        
+                        $opts[$key][] = $arg;
+                    } else {
+                        $opts[$key] = $arg;
+                    }
+                    
+                    $key = '';
+                }
+            }
+ 
+            return $opts;
+        }
+        
+        /**
          * Determine available "commands".
          *
          * @octdoc  m:main/getCommands
@@ -86,7 +157,7 @@ namespace octris {
             $this->getCommands();
             
             array_shift($argv);
-            
+
             if (!($arg = array_shift($argv))) {
                 $this->showUsage();
                 exit(1);
@@ -115,18 +186,18 @@ namespace octris {
                         exit(1);
                     } else {
                         $class = "\\octris\\command\\$arg";
-                        
+
                         if ($help) {
                             print trim($class::getManual(), "\n") . "\n";
                             exit(1);
                         } else {
-                            provider::set('args', \org\octris\core\app\cli::getOptions($argv));
-                            
+                            provider::set('args', $this->getOptions($argv));
+
                             $instance = new $class();
-                        
+
                             if (($return = $instance->run())) {
                                 printf("**error** %s\n", rtrim($instance->getError(), "\n"));
-                                
+
                                 exit($return);
                             }
                         }
