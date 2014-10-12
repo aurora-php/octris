2d4c3e405b847609a11a0ed45b8fa9c6908bf517
Harald Lapp <harald@octris.org>
2014-10-12 21:33:54 +0200

    initial commit doc tool

---
+++ libs/command/doc.class.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of the 'octris' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace octris\command {
+    use \org\octris\core\provider as provider;
+    use \org\octris\core\validate as validate;
+
+    /**
+     * Documentation tools.
+     *
+     * @octdoc      c:command/doc
+     * @copyright   copyright (c) 2014 by Harald Lapp
+     * @author      Harald Lapp <harald@octris.org>
+     */
+    class doc extends \octris\command
+    /**/
+    {
+        /**
+         * Run command.
+         *
+         * @octdoc  m:lint/run
+         */
+        public function run()
+        /**/
+        {
+            // $this->setError('not implemented, yet');
+            //
+            // return 1;
+
+            // export EBNF
+            $grammar = new \org\octris\core\tpl\compiler\grammar();
+            print $grammar->getEBNF();
+        }
+    }
+}
