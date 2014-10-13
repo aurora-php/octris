857caf93bf48abae2cff18d745305b0ec1927950
Harald Lapp <harald@octris.org>
2014-10-13 03:46:30 +0200

    allow to use configuration files from cli app

--- data/skel/cli/$module.php
+++ data/skel/cli/$module.php
@@ -27,5 +27,14 @@
 
 require_once(__DIR__ . '/libs/autoloader.class.php');
 
+// load application configuration
+$registry = \org\octris\core\registry::getInstance();
+$registry->set('OCTRIS_APP', '{{$directory}}', \org\octris\core\registry::T_READONLY);
+$registry->set('OCTRIS_BASE', __DIR__, \org\octris\core\registry::T_READONLY);
+$registry->set('config', function() {
+    return new \org\octris\core\config('{{$directory}}', 'config');
+}, \org\octris\core\registry::T_SHARED | \org\octris\core\registry::T_READONLY);
+
+// run application
 $main = new {{$module}}\main();
 $main->run();
--- octris.php
+++ octris.php
@@ -26,5 +26,14 @@ if (version_compare(PHP_VERSION, '5.6.0') < 0) {
 
 require_once(__DIR__ . '/libs/autoloader.class.php');
 
+// load application configuration
+$registry = \org\octris\core\registry::getInstance();
+$registry->set('OCTRIS_APP', 'octris', \org\octris\core\registry::T_READONLY);
+$registry->set('OCTRIS_BASE', __DIR__, \org\octris\core\registry::T_READONLY);
+$registry->set('config', function() {
+    return new \org\octris\core\config('octris', 'config');
+}, \org\octris\core\registry::T_SHARED | \org\octris\core\registry::T_READONLY);
+
+// run application
 $main = new \octris\main();
 $main->run();
