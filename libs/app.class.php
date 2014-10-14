7925227d2b06b3e9f4ff444aba31fbaf5840310a
Harald Lapp <harald@octris.org>
2014-10-14 23:56:01 +0200

    cleanup, most code is now implemented by cliff

--- libs/app.class.php
+++ libs/app.class.php
@@ -11,7 +11,6 @@
 
 namespace octris {
     use \org\octris\core\provider as provider;
-    use \org\octris\cliff\options as options;
 
     /**
      * Application class.
@@ -51,15 +50,6 @@ namespace octris {
         /**/
         
         /**
-         * Available commands.
-         *
-         * @octdoc  p:app/$commands
-         * @type    array
-         */
-        protected $commands = array();
-        /**/
-
-        /**
          * Constructor.
          *
          * @octdoc  m:app/__construct
@@ -71,89 +61,29 @@ namespace octris {
         }
 
         /**
-         * Determine available "commands".
+         * Configure application arguments.
          *
-         * @octdoc  m:app/getCommands
+         * @octdoc  m:app/configure
+         * @param   \org\octris\cliff\options       $options            Instance of options parser.
          */
-        protected function getCommands()
+        protected function configure(\org\octris\cliff\options $options)
         /**/
         {
-            $this->commands = array();
+            parent::configure($options);
             
-            foreach (new \DirectoryIterator(__DIR__ . '/command/') as $file) {
-                if (!$file->isDot() && substr(($name = $file->getFilename()), -4) == '.php') {
-                    $command = basename($name, '.class.php');
-                    
-                    $this->commands[$command] = '\\octris\\command\\' . $command;
-                }
-            }
-            
-            ksort($this->commands);
+            // create
+            $options->addCommand('create', new \octris\command\create());
         }
-    
+
         /**
          * Run main application.
          *
          * @octdoc  m:app/main
          * @param   \org\octris\cliff\options\collection        $args           Parsed arguments.
          */
-        public function main(\org\octris\cliff\options\collection $args)
+        protected function main(\org\octris\cliff\options\collection $args)
         /**/
         {
-            // $this->getCommands();
-            //
-            // $opts = new \org\octris\cliff\options();
-            // $opts->addOption(['h', 'help'])->setAction(function() {
-            //     $this->showUsage();
-            //     exit(1);
-            // });
-            // $opts->addOption(['version'])->setAction(function() {
-            //     printf("octris %s (%s)\n", self::T_VERSION, self::T_VERSION_DATE);
-            //     exit(1);
-            // });
-            //
-            // // help command
-            // $opts->addCommand('help')->setAction(function(\org\octris\cliff\options\collection $collection) {
-            //     if (count($operands = $collection->getOperands()) != 1) {
-            //         $this->showUsage();
-            //         exit(1);
-            //     }
-            //
-            //     if (!isset($this->commands[$operands[0]])) {
-            //         printf("octris: '%s' is not a command\n", $operands[0]);
-            //         exit(1);
-            //     }
-            //
-            //     $class = '\\octris\\command\\' . $operands[0];
-            //
-            //     print trim($class::getManual(), "\n") . "\n";
-            //     exit(1);
-            // });
-            //
-            // // create
-            // $cmd = $opts->addCommand('create')->setAction(function(\org\octris\cliff\options\collection $collection) {
-            //     $cmd = new \octris\command\create($collection);
-            //
-            //     if (($return = $cmd->run())) {
-            //         printf("**error** %s\n", rtrim($cmd->getError(), "\n"));
-            //         exit(1);
-            //     }
-            // });
-            // $cmd->addOption(['p', 'project'], options::T_VALUE | options::T_REQUIRED)->setValidator(function($value) {
-            //     $validator = new \org\octris\core\validate\type\project();
-            //     return $validator->validate($validator->preFilter($value));
-            // }, 'invalid project name specified');
-            // $cmd->addOption(['t', 'type'], options::T_VALUE | options::T_REQUIRED)->setValidator(function($value) {
-            //     return in_array($value, ['web', 'cli', 'lib']);
-            // }, 'invalid project type specified');
-            // $cmd->addOption(['d', 'define'], options::T_KEYVALUE)->setValidator(function($value, $key) {
-            //     return (in_array($key, ['info.company', 'info.author', 'info.email']) && $value != '');
-            // }, 'invalid argument value');
-            //
-            // if (!$opts->process()) {
-            //     exit(1);
-            // }
-
             exit(0);
         }
     
@@ -162,7 +92,7 @@ namespace octris {
          *
          * @octdoc  m:app/showHelp
          */
-        public function showHelp()
+        protected function showHelp()
         /**/
         {
             printf("               __         .__        
@@ -170,27 +100,9 @@ namespace octris {
  /  _ \_/ ___\   __\_  __ \  |/  ___/    OCTRiS framework tool
 (  <_> )  \___|  |  |  | \/  |\___ \     copyright (c) 2014 by Harald Lapp
  \____/ \___  >__|  |__|  |__/____  >    http://github.com/octris/octris/
-            \/%20s\/
+            \/%20s\/\n\n", 'v' . static::$app_version);
 
-usage: octris --help
-usage: octris --version
-usage: octris help <command>
-usage: octris <command> [ARGUMENTS]
-
-Commands:
-", 'v' . static::$app_version);
-
-            $size = array_reduce(array_keys($this->commands), function($size, $item) {
-                return max($size, strlen($item));
-            }, 0);
-
-            foreach ($this->commands as $command => $class) {
-                if (($desc = $class::getDescription()) == '') {
-                    continue;
-                }
-                
-                printf("    %-" . $size . "s    %s\n", $command, $class::getDescription());
-            }
+            parent::showHelp();
         }
     }
 
