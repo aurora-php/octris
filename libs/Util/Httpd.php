90d5e28c1d6f305ed9961c89c70ef2698aaa182a
Harald Lapp <harald@octris.org>
2014-11-18 23:48:11 +0100

    initial commit httpd util library

---
+++ libs/Util/Httpd.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the octris/octris.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Octris\Util;
+
+use \Octris\Core\Provider as provider;
+use \Octris\Core\Validate as validate;
+use \Octris\Cliff\Args    as args;
+
+/**
+ * Customization for httpd library.
+ *
+ * @copyright   copyright (c) 2014 by Harald Lapp
+ * @author      Harald Lapp <harald@octris.org>
+ */
+class Httpd extends \Octris\Httpd
+{
+}
