bc5fd324f377a8ad8a1288315666234d48c5c3c8
Harald Lapp <harald@octris.org>
2015-05-03 12:15:25 +0200

    bumped version

--- libs/App.php
+++ libs/App.php
@@ -34,7 +34,7 @@ class App extends \Octris\Cliff\App
      *
      * @type    string
      */
-    protected static $app_version = '0.0.7';
+    protected static $app_version = '0.0.8';
     /**/
 
     /**
@@ -42,7 +42,7 @@ class App extends \Octris\Cliff\App
      *
      * @type    string
      */
-    protected static $app_version_date = '2015-05-01';
+    protected static $app_version_date = '2015-05-03';
     /**/
 
     /**
