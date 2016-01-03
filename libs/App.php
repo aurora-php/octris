0b000e021bdf20699ed565b34720239efa61a46a
Harald Lapp <harald@octris.org>
2016-01-03 22:05:38 +0100

    bumped version for bugfix release

--- libs/App.php
+++ libs/App.php
@@ -34,7 +34,7 @@ class App extends \Octris\Cliff\App
      *
      * @type    string
      */
-    protected static $app_version = '0.0.11';
+    protected static $app_version = '0.0.12';
     /**/
 
     /**
