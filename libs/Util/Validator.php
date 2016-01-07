806211c7d5f1aae8b0f7d16cf3bdd523350046fb
Harald Lapp <harald@octris.org>
2016-01-08 00:29:50 +0100

    added check to test if project is a web application

--- libs/Util/Validator.php
+++ libs/Util/Validator.php
@@ -33,4 +33,18 @@ class Validator
             return is_file($value . '/etc/global.php');
         }, 'global app configuration not found "${value}/etc/global.php"');
     }
+
+    /**
+     * Add check for valid web application project path.
+     *
+     * @param   object              $object                 Object to add checks to.
+     */
+    public static function addWebProjectPathCheck($object)
+    {
+        self::addProjectPathCheck($object);
+
+        $object->addValidator(function($value) {
+            return (is_dir($value . '/libs/app') && is_file($value . '/libs/app/entry.php'));
+        }, 'Specified path does not seem to belong to a web application created with the OCTRiS framework');
+    }
 }
