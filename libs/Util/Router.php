317870a6e2930d1399eac6475703f59c9c2fb389
Harald Lapp <harald@octris.org>
2016-01-31 16:20:36 +0100

    improved router
    to prevent listing of directorie index and to handle mime-types that the php built-in webserver does not know

--- libs/Util/Router.php
+++ libs/Util/Router.php
@@ -13,11 +13,38 @@
  * Router script for httpd command.
  */
 
-ob_end_clean();
+function octris_router() {
+    $known_exts = ['3gp', 'apk', 'avi', 'bmp', 'css', 'csv', 'doc', 'docx', 'flac', 'gif', 'gz', 'gzip', 'htm', 'html', 'ics', 'jpe', 'jpeg', 'jpg', 'js', 'kml', 'kmz', 'm4a', 'mov', 'mp3', 'mp4', 'mpeg', 'mpg', 'odp', 'ods', 'odt', 'oga', 'ogg', 'ogv', 'pdf', 'pdf', 'png', 'pps', 'pptx', 'qt', 'svg', 'swf', 'tar', 'text', 'tif', 'txt', 'wav', 'webm', 'wmv', 'xls', 'xlsx', 'xml', 'xsl', 'xsd', 'zip'];
 
-if (file_exists($_SERVER["DOCUMENT_ROOT"] . $_SERVER["REQUEST_URI"])) {
-    // serve static files
-    return false;
+    $path = $_SERVER["DOCUMENT_ROOT"] . parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+    $name = basename($path);
+
+    if (file_exists($path) && $name != 'index.php' && !is_file($path . '/index.php')) {
+        if (is_dir($path)) {
+            // forbidden to list directory index
+            http_response_code(403);
+
+            print '<h1>403</h1>';
+        } else {
+            $ext = pathinfo($path, PATHINFO_EXTENSION);
+
+            if (in_array($ext, $known_exts)) {
+                // built-in webserver can handle file extension
+                return false;
+            } else {
+                // unhandled file extension
+                $mime_type = mime_content_type($path);
+
+                header('Content-type: ' . $mime_type);
+
+                readfile($path);
+            }
+        }
+    } else {
+        require_once($_SERVER['DOCUMENT_ROOT'] . '/index.php');
+    }
 }
 
-require_once($_SERVER['DOCUMENT_ROOT'] . '/index.php');
+ob_end_clean();
+
+return octris_router();
